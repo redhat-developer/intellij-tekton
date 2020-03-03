2241408dd29db46d6255f2b39b3aea99c003f639
Jeff MAURY <jmaury@redhat.com>
2020-03-03 12:19:01 +0100

    Add first integration test
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/it/java/com/redhat/devtools/intellij/tektoncd/BaseTest.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2019 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.tektoncd;
+
+import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.ui.TestDialog;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
+import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import com.redhat.devtools.intellij.tektoncd.tkn.Tkn;
+import com.redhat.devtools.intellij.tektoncd.tkn.TknCli;
+import io.fabric8.kubernetes.client.ConfigBuilder;
+import io.fabric8.kubernetes.client.DefaultKubernetesClient;
+import io.fabric8.kubernetes.client.KubernetesClient;
+import org.junit.After;
+import org.junit.Before;
+
+public abstract class BaseTest {
+    private CodeInsightTestFixture myFixture;
+    private TestDialog previousTestDialog;
+    protected Tkn tkn;
+    protected KubernetesClient client;
+
+    @Before
+    public void setUp() throws Exception {
+        IdeaTestFixtureFactory factory = IdeaTestFixtureFactory.getFixtureFactory();
+        TestFixtureBuilder<IdeaProjectTestFixture> fixtureBuilder = factory.createLightFixtureBuilder();
+        IdeaProjectTestFixture fixture = fixtureBuilder.getFixture();
+        myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(fixture);
+        myFixture.setUp();
+        previousTestDialog = Messages.setTestDialog(new TestDialog() {
+            @Override
+            public int show(String message) {
+                return 0;
+            }
+        });
+        tkn = TknCli.get();
+        client = new DefaultKubernetesClient(new ConfigBuilder().build());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        myFixture.tearDown();
+        Messages.setTestDialog(previousTestDialog);
+    }
+}
