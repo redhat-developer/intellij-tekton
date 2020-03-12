c9d6bd939d24258d938a2069a8a5efd313991e15
Luca Stocchi <lstocchi@redhat.com>
2020-03-12 13:18:16 +0100

    added notification to notify users regarding save (#16)
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/tektoncd/listener/SaveInEditorListener.java
+++ src/main/java/com/redhat/devtools/intellij/tektoncd/listener/SaveInEditorListener.java
@@ -3,9 +3,12 @@ package com.redhat.devtools.intellij.tektoncd.listener;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Strings;
+import com.intellij.notification.Notification;
+import com.intellij.notification.NotificationType;
 import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
 import com.intellij.openapi.fileEditor.FileDocumentSynchronizationVetoer;
+import com.intellij.openapi.project.ProjectManager;
 import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.redhat.devtools.intellij.common.utils.UIHelper;
@@ -14,6 +17,7 @@ import com.redhat.devtools.intellij.tektoncd.utils.JSONHelper;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.dsl.base.CustomResourceDefinitionContext;
+import org.apache.commons.lang.StringUtils;
 import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -46,19 +50,24 @@ public class SaveInEditorListener extends FileDocumentSynchronizationVetoer {
             return false;
         }
 
+        Notification notification;
         try (final KubernetesClient client = new DefaultKubernetesClient()) {
             JsonNode specToUpload = JSONHelper.getSpecJSON(document.getText());
-            CustomResourceDefinitionContext crdContext = CRDHelper.getCRDContext(vf.getUserData((KIND_PLURAL)));
+            CustomResourceDefinitionContext crdContext = CRDHelper.getCRDContext(vf.getUserData(KIND_PLURAL));
             JsonNode customResource = JSONHelper.MapToJSON(client.customResource(crdContext).get(namespace, name));
             ((ObjectNode) customResource).set("spec", specToUpload);
             client.customResource(crdContext).edit(namespace, name, customResource.toString());
         } catch (IOException e) {
-            logger.error("Error: " + e.getLocalizedMessage());
-            return false;
-        } catch (Exception e) {
+            // give a visual notification to user if an error occurs during saving
+            notification = new Notification("SaveNotification", "Error", "An error occurred while saving " + vf.getUserData(KIND_PLURAL) + " " + name, NotificationType.ERROR);
+            notification.notify(ProjectManager.getInstance().getDefaultProject());
             logger.error("Error: " + e.getLocalizedMessage());
             return false;
         }
+
+        // notify user if saving was completed successfully
+        notification = new Notification("SaveNotification", "Save Successful", StringUtils.capitalize(vf.getUserData(KIND_PLURAL)) + " " + name + " has been saved!", NotificationType.INFORMATION);
+        notification.notify(ProjectManager.getInstance().getDefaultProject());
         return true;
     }
 }
