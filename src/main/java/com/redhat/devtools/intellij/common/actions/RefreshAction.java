32ad1c6a1847847dfab6638866ef09cc3eea2515
Luca Stocchi <lstocchi@redhat.com>
2020-03-18 10:57:22 +0100

    extend refresh to pipeline/task children nodes (#26)
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/common/actions/RefreshAction.java
+++ src/main/java/com/redhat/devtools/intellij/common/actions/RefreshAction.java
@@ -2,15 +2,14 @@ package com.redhat.devtools.intellij.common.actions;
 
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.redhat.devtools.intellij.common.tree.LazyMutableTreeNode;
-import com.redhat.devtools.intellij.tektoncd.tree.ClusterTasksNode;
-import com.redhat.devtools.intellij.tektoncd.tree.PipelinesNode;
-import com.redhat.devtools.intellij.tektoncd.tree.ResourcesNode;
-import com.redhat.devtools.intellij.tektoncd.tree.TasksNode;
+import com.redhat.devtools.intellij.tektoncd.tree.*;
 
 import javax.swing.tree.TreePath;
 
 public class RefreshAction extends TreeAction {
-    public RefreshAction() { super(PipelinesNode.class, TasksNode.class, ClusterTasksNode.class, ResourcesNode.class); }
+    public RefreshAction() {
+        super(PipelinesNode.class, TasksNode.class, ClusterTasksNode.class, ResourcesNode.class, PipelineNode.class, TaskNode.class);
+    }
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected) {
