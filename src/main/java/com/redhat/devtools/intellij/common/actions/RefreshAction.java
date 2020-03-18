a2a681b517fc88644c009c42d6226c54203a9161
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2020-03-18 15:23:27 +0100

    Added refresh action (#26) (#27)
    * added refresh action to second level nodes (#26)
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * extend refresh to pipeline/task children nodes (#26)
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * added header (#26)
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

---
+++ src/main/java/com/redhat/devtools/intellij/common/actions/RefreshAction.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc.
+ ******************************************************************************/
+package com.redhat.devtools.intellij.common.actions;
+
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.redhat.devtools.intellij.common.tree.LazyMutableTreeNode;
+import com.redhat.devtools.intellij.tektoncd.tree.*;
+
+import javax.swing.tree.TreePath;
+
+public class RefreshAction extends TreeAction {
+    public RefreshAction() {
+        super(PipelinesNode.class, TasksNode.class, ClusterTasksNode.class, ResourcesNode.class, PipelineNode.class, TaskNode.class);
+    }
+
+    @Override
+    public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected) {
+        ((LazyMutableTreeNode) selected).reload();
+    }
+}
