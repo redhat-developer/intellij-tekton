d643fee292ccee67fe667073575156fa7cd1262d
Luca Stocchi <lstocchi@redhat.com>
2020-04-11 11:34:01 +0200

    removed equal check
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/tektoncd/listener/SaveInEditorListener.java
+++ src/main/java/com/redhat/devtools/intellij/tektoncd/listener/SaveInEditorListener.java
@@ -61,7 +61,7 @@ public class SaveInEditorListener extends FileDocumentSynchronizationVetoer {
         Long currentModificationStamp = document.getModificationStamp();
         if(project == null ||
                  !isFileToPush(project, document, vf) ||
-                 (lastModificationStamp != null && currentModificationStamp.equals(lastModificationStamp))
+                 currentModificationStamp.equals(lastModificationStamp)
         ) {
             return true;
         }
