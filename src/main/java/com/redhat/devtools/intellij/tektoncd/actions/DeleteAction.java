7986bf8fa9cb39699be1cbcbaf88b5775b2e055c
Luca Stocchi <49404737+lstocchi@users.noreply.github.com>
2020-06-09 16:22:34 +0200

    added delete on multiple items (#83) (#124)
    * added delete on multiple items (#83)
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>
    
    * changes based on review
    
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/tektoncd/actions/DeleteAction.java
+++ src/main/java/com/redhat/devtools/intellij/tektoncd/actions/DeleteAction.java
@@ -32,49 +32,63 @@ import com.redhat.devtools.intellij.tektoncd.tree.TriggerTemplateNode;
 
 import javax.swing.tree.TreePath;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.concurrent.CompletableFuture;
 
 public class DeleteAction extends TektonAction {
-    public DeleteAction() { super(TaskNode.class, PipelineNode.class, ResourceNode.class, ClusterTaskNode.class, ConditionNode.class, TriggerTemplateNode.class, TriggerBindingNode.class, ClusterTriggerBindingNode.class, EventListenerNode.class); }
+    public DeleteAction() { super(true, TaskNode.class, PipelineNode.class, ResourceNode.class, ClusterTaskNode.class, ConditionNode.class, TriggerTemplateNode.class, TriggerBindingNode.class, ClusterTriggerBindingNode.class, EventListenerNode.class); }
 
     @Override
-    public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected, Tkn tkncli) {
-        ParentableNode<? extends ParentableNode<NamespaceNode>> element = getElement(selected);
+    public void actionPerformed(AnActionEvent anActionEvent, TreePath[] path, Object[] selected, Tkn tkncli) {
+        ParentableNode<? extends ParentableNode<NamespaceNode>>[] elements = Arrays.stream(selected).map(item -> getElement(item)).toArray(ParentableNode[]::new);
         int resultDialog = UIHelper.executeInUI(() -> {
-            String kind = element.getClass().getSimpleName().toLowerCase().replace("node", "");
-            return Messages.showYesNoDialog("Are you sure you want to delete " + kind + " " + element.getName() + " ?",
-                    "Delete " + element.getName(),
+            String name = "";
+            String dialogText = "";
+            if (elements.length == 1) {
+                name = elements[0].getName();
+                dialogText = elements[0].getClass().getSimpleName().toLowerCase().replace("node", "") + " " + name + " ?";
+            } else {
+                dialogText = "the following items?\n";
+                for (ParentableNode element: elements) {
+                    dialogText += element.getName() + "\n";
+                }
+            }
+
+            return Messages.showYesNoDialog("Are you sure you want to delete " + dialogText,
+                    "Delete " + name,
                     null
             );
         });
 
         CompletableFuture.runAsync(() -> {
-            try {
-                if (resultDialog == Messages.OK) {
-                    String namespace = element.getParent().getParent().getName();
-                    if (element instanceof PipelineNode) {
-                        tkncli.deletePipeline(namespace, element.getName());
-                    } else if (element instanceof ResourceNode) {
-                        tkncli.deleteResource(namespace, element.getName());
-                    } else if (element instanceof TaskNode) {
-                        tkncli.deleteTask(namespace, element.getName());
-                    } else if (element instanceof  ClusterTaskNode) {
-                        tkncli.deleteClusterTask(element.getName());
-                    } else if (element instanceof ConditionNode) {
-                        tkncli.deleteCondition(namespace, element.getName());
-                    } else if (element instanceof TriggerTemplateNode) {
-                        tkncli.deleteTriggerTemplate(namespace, element.getName());
-                    } else if (element instanceof TriggerBindingNode) {
-                        tkncli.deleteTriggerBinding(namespace, element.getName());
-                    } else if (element instanceof  ClusterTriggerBindingNode) {
-                        tkncli.deleteClusterTriggerBinding(namespace, element.getName());
-                    } else if (element instanceof  EventListenerNode) {
-                        tkncli.deleteEventListener(namespace, element.getName());
+            if (resultDialog == Messages.OK) {
+                for(ParentableNode<? extends ParentableNode<NamespaceNode>> element : elements) {
+                    try {
+                        String namespace = element.getNamespace();
+                        if (element instanceof PipelineNode) {
+                            tkncli.deletePipeline(namespace, element.getName());
+                        } else if (element instanceof ResourceNode) {
+                            tkncli.deleteResource(namespace, element.getName());
+                        } else if (element instanceof TaskNode) {
+                            tkncli.deleteTask(namespace, element.getName());
+                        } else if (element instanceof ClusterTaskNode) {
+                            tkncli.deleteClusterTask(element.getName());
+                        } else if (element instanceof ConditionNode) {
+                            tkncli.deleteCondition(namespace, element.getName());
+                        } else if (element instanceof TriggerTemplateNode) {
+                            tkncli.deleteTriggerTemplate(namespace, element.getName());
+                        } else if (element instanceof TriggerBindingNode) {
+                            tkncli.deleteTriggerBinding(namespace, element.getName());
+                        } else if (element instanceof ClusterTriggerBindingNode) {
+                            tkncli.deleteClusterTriggerBinding(namespace, element.getName());
+                        } else if (element instanceof EventListenerNode) {
+                            tkncli.deleteEventListener(namespace, element.getName());
+                        }
+                        ((TektonTreeStructure) getTree(anActionEvent).getClientProperty(Constants.STRUCTURE_PROPERTY)).fireModified(element.getParent());
+                    } catch (IOException e) {
+                        UIHelper.executeInUI(() -> Messages.showErrorDialog("Error: " + e.getLocalizedMessage(), "Error"));
                     }
-                    ((TektonTreeStructure)getTree(anActionEvent).getClientProperty(Constants.STRUCTURE_PROPERTY)).fireModified(element.getParent());
                 }
-            } catch (IOException e) {
-                UIHelper.executeInUI(() -> Messages.showErrorDialog("Error: " + e.getLocalizedMessage(), "Error"));
             }
         });
 
