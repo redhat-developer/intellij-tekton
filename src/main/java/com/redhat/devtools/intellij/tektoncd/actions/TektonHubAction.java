af64928d1b7a1171901c7d8fd21c558a670d9a07
Andre Dietisheim <adietish@redhat.com>
2021-03-12 16:08:02 +0100

    added telemetry to TektonHubAction
    Signed-off-by: Andre Dietisheim <adietish@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/tektoncd/actions/TektonHubAction.java
+++ src/main/java/com/redhat/devtools/intellij/tektoncd/actions/TektonHubAction.java
@@ -15,6 +15,7 @@ import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
 import com.redhat.devtools.intellij.common.utils.ExecHelper;
 import com.redhat.devtools.intellij.common.utils.UIHelper;
+import com.redhat.devtools.intellij.tektoncd.telemetry.TelemetryService;
 import com.redhat.devtools.intellij.tektoncd.tkn.Tkn;
 import com.redhat.devtools.intellij.tektoncd.tree.ClusterTasksNode;
 import com.redhat.devtools.intellij.tektoncd.tree.ParentableNode;
@@ -25,11 +26,14 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.swing.tree.TreePath;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
 import static com.redhat.devtools.intellij.tektoncd.Constants.HUB_CATALOG_TAG;
+import static com.redhat.devtools.intellij.telemetry.core.service.TelemetryMessageBuilder.ActionMessage;
+import static com.redhat.devtools.intellij.telemetry.core.util.AnonymizeUtils.anonymizeResource;
 
 public class TektonHubAction extends TektonAction {
     Logger logger = LoggerFactory.getLogger(TektonHubAction.class);
@@ -38,6 +42,7 @@ public class TektonHubAction extends TektonAction {
 
     @Override
     public void actionPerformed(AnActionEvent anActionEvent, TreePath path, Object selected, Tkn tkncli) {
+        ActionMessage telemetry = TelemetryService.instance().action("tekton hub");
         ExecHelper.submit(() -> {
             ParentableNode element = getElement(selected);
             String namespace = element.getNamespace();
@@ -51,23 +56,24 @@ public class TektonHubAction extends TektonAction {
                         .filter(ct -> ct.getMetadata().getLabels() != null && ct.getMetadata().getLabels().containsKey(HUB_CATALOG_TAG))
                         .map(ct -> ct.getMetadata().getName())
                         .collect(Collectors.toList());
+                Project project = getEventProject(anActionEvent);
+                HubModel model = new HubModel(project, tkncli, namespace, tasks, clusterTasks, element instanceof TasksNode);
+                telemetry.send();
+                UIHelper.executeInUI(() -> {
+                    HubDialog wizard = new HubDialog(project, model);
+                    wizard.setModal(false);
+                    wizard.show();
+                    return wizard;
+                });
             } catch (IOException e) {
+                String errorMessage = "Failed to retrieve data for " + element.getName() + " in namespace " + namespace + ". An error occurred while retrieving them.\n" + e.getLocalizedMessage();
+                telemetry.error(anonymizeResource(element.getName(), namespace, errorMessage)).send();
                 UIHelper.executeInUI(() ->
                         Messages.showErrorDialog(
-                                "Failed to retrieve data for " + element.getName() + " in namespace " + namespace + ". An error occurred while retrieving them.\n" + e.getLocalizedMessage(),
+                                errorMessage,
                                 "Error"));
                 logger.warn("Error: " + e.getLocalizedMessage());
-                return;
             }
-
-            Project project = getEventProject(anActionEvent);
-            HubModel model = new HubModel(project, tkncli, namespace, tasks, clusterTasks, element instanceof TasksNode);
-            UIHelper.executeInUI(() -> {
-                HubDialog wizard = new HubDialog(project, model);
-                wizard.setModal(false);
-                wizard.show();
-                return wizard;
-            });
         });
 
     }
