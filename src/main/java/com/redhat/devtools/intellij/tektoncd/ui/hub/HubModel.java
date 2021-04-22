e058975d4fea46f11a12fde9b988ccb8e2aa84c6
Luca Stocchi <lstocchi@redhat.com>
2021-04-22 17:16:42 +0200

    add test for deployHelper class
    Signed-off-by: Luca Stocchi <lstocchi@redhat.com>

--- src/main/java/com/redhat/devtools/intellij/tektoncd/ui/hub/HubModel.java
+++ src/main/java/com/redhat/devtools/intellij/tektoncd/ui/hub/HubModel.java
@@ -182,7 +182,7 @@ public class HubModel {
             }
             yamlObject.put("kind", kind);
             String yamlUpdated = YAMLBuilder.writeValueAsString(yamlObject);
-            if (DeployHelper.saveOnCluster(project, yamlUpdated, confirmationMessage, true, false)) {
+            if (DeployHelper.saveOnCluster(project, "", yamlUpdated, confirmationMessage, true, false)) {
                 if (!clusterTaskAlreadyOnCluster) {
                     clusterTasksInstalled.add(name);
                     return Constants.InstallStatus.INSTALLED;
