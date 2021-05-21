e309123bdbb0e9fb3c362883df2bae81888d5eee
Jeff MAURY <jmaury@redhat.com>
2021-05-21 12:13:25 +0200

    Add unit test
    Signed-off-by: Jeff MAURY <jmaury@redhat.com>

---
+++ src/test/java/com/redhat/devtools/intellij/tektoncd/schemas/TriggerTemplateSchemasTest.java
@@ -0,0 +1,52 @@
+/*******************************************************************************
+ * Copyright (c) 2020 Red Hat, Inc.
+ * Distributed under license by Red Hat, Inc. All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v2.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v20.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ ******************************************************************************/
+package com.redhat.devtools.intellij.tektoncd.schemas;
+
+import com.intellij.openapi.vfs.newvfs.impl.VfsRootAccess;
+import com.intellij.testFramework.fixtures.CodeInsightTestFixture;
+import com.intellij.testFramework.fixtures.IdeaProjectTestFixture;
+import com.intellij.testFramework.fixtures.IdeaTestFixtureFactory;
+import com.intellij.testFramework.fixtures.TestFixtureBuilder;
+import org.jetbrains.yaml.schema.YamlJsonSchemaHighlightingInspection;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+public class TriggerTemplateSchemasTest {
+    private CodeInsightTestFixture myFixture;
+
+    @Before
+    public void setup() throws Exception {
+        IdeaTestFixtureFactory factory = IdeaTestFixtureFactory.getFixtureFactory();
+        TestFixtureBuilder<IdeaProjectTestFixture> fixtureBuilder = factory.createLightFixtureBuilder(null);
+        IdeaProjectTestFixture fixture = fixtureBuilder.getFixture();
+
+        myFixture = IdeaTestFixtureFactory.getFixtureFactory().createCodeInsightFixture(fixture, factory.createTempDirTestFixture());
+
+        myFixture.setTestDataPath("src/test/resources");
+        myFixture.setUp();
+        myFixture.enableInspections(YamlJsonSchemaHighlightingInspection.class);
+        VfsRootAccess.allowRootAccess(new File("src").getAbsoluteFile().getParentFile().getAbsolutePath());
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        myFixture.tearDown();
+    }
+
+    @Test
+    public void testTriggerTemplate() {
+        myFixture.configureByFile("schemas/trigger-template1.yaml");
+        myFixture.checkHighlighting();
+    }
+}
